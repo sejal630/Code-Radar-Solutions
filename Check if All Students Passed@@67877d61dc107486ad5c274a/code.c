31296c45e5b86e4f4de90b597e16c34eaae3db9b
Sejal <sejusaini4006@gmail.com>
2025-04-10 13:06:52 +0530

    Solution to Check if All Students Passed

---
+++ Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct student{
+    int roll;
+    char name;
+    float marks;
+};
+int main(){
+    int n,i,allPassed=1;
+    scanf("%d",&n);
+    struct student students[n];
+    for(i=0;i<n;i++){
+        scanf("%d %s %f",&students[i].roll,&student[i].name,&student[i].marks);
+        if(students[i].marks<=50){
+        allPassed=0;
+        }
+    }
+    if(allPassed){
+        printf("All Passed\n");
+    }
+    else{
+        printf("Not All Passes\n");
+    }
+    return 0;
+}
